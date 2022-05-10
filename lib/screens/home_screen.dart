88be8ddf551c486f90bf56cd6cd666d406a071ce
Nicolette Pascual <nicolette.pascual@gmail.com>
2022-05-10 23:29:52 +0800

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+             Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
